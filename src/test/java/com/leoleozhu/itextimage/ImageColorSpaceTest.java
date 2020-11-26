a77e06f40bf194cce45b0df95bb13d5fbe55595c
leo <Leiping.Zhu@gmail.com>
2020-11-26 15:29:24 +0800

    add mask to imagedata, need to be improved

--- src/test/java/com/leoleozhu/itextimage/ImageColorSpaceTest.java
+++ src/test/java/com/leoleozhu/itextimage/ImageColorSpaceTest.java
@@ -15,6 +15,11 @@ import com.itextpdf.layout.element.Image;
 import com.leoleozhu.utils.TestCaseBase;
 import org.junit.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
 public class ImageColorSpaceTest extends TestCaseBase {
 
     /**
@@ -69,7 +74,6 @@ public class ImageColorSpaceTest extends TestCaseBase {
         pdfCanvas.release();
         pdfDocument.close();
     }
-
 
     /**
      * Crop center of the image with the display area and add to pdf page
@@ -135,4 +139,84 @@ public class ImageColorSpaceTest extends TestCaseBase {
         pdfDocument.close();
     }
 
+    /**
+     * Crop center of the image with the display area, add a mask on it and add to pdf page.
+     * However, the mask is applied on the original imageData instead of the cropped one.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddImageToPdfWithPdfCanvasCropCenterAndMask() throws Exception {
+
+        String imageWithIcc = resourceFile("images/image-ios-profile.jpg");
+        String imageMask = resourceFile("masks/circle.png");
+        String destination = targetFile("image-colorspace-AddImageToPdfWithPdfCanvasCropCenterAndMask.pdf");
+
+        float margin = mm2pt(10f);
+        float dspWidth = mm2pt(160f);
+        float dspHeight = mm2pt(160f);
+        float pageWidth = dspWidth + margin * 2;
+        float pageHeight = dspHeight + margin * 2;
+
+        PdfDocument pdfDocument = new PdfDocument(new PdfWriter(destination));
+        PdfPage page = pdfDocument.addNewPage(new PageSize(pageWidth, pageHeight));
+
+        // create cropped image xObject
+        ImageData imageData = ImageDataFactory.create(imageWithIcc);
+
+        // create mask
+        ImageData maskData = makeBlackAndWhitePng(imageMask);
+        maskData.makeMask();
+        imageData.setImageMask(maskData);
+
+        Image img = new Image(imageData);
+
+        float imgWidth = imageData.getWidth();
+        float imgHeight = imageData.getHeight();
+        float cropX, cropY, cropWidth, cropHeight;
+
+        if (imgWidth / dspWidth > imgHeight / dspHeight) {
+            cropHeight = imgHeight;
+            cropWidth = imgHeight / dspHeight * dspWidth;
+            cropX = (imgWidth - cropWidth) / 2;
+            cropY = 0;
+        } else {
+            cropWidth = imgWidth;
+            cropHeight = imgWidth / dspWidth * dspHeight;
+            cropX = 0;
+            cropY = (imgHeight - cropHeight) / 2;
+        }
+
+        img.setFixedPosition(-cropX, -cropY);
+
+        PdfFormXObject croppedImage = new PdfFormXObject(new Rectangle(cropWidth, cropHeight));
+
+        Canvas canvas = new Canvas(croppedImage, pdfDocument);
+        canvas.add(img);
+
+        // create page canvas
+        PdfCanvas pdfCanvas = new PdfCanvas(page);
+
+        // create AT
+        AffineTransform at = AffineTransform.getTranslateInstance(margin, margin);
+        at.concatenate(AffineTransform.getScaleInstance(dspWidth / croppedImage.getWidth(), dspHeight / croppedImage.getHeight()));
+
+        float[] matrix = new float[6];
+        at.getMatrix(matrix);
+
+        // add cropped image
+        pdfCanvas.addXObjectWithTransformationMatrix(croppedImage, matrix[0], matrix[1], matrix[2], matrix[3], matrix[4], matrix[5]);
+
+        pdfCanvas.release();
+        pdfDocument.close();
+    }
+
+    private static ImageData makeBlackAndWhitePng(String image) throws Exception {
+        BufferedImage bi = ImageIO.read(new File(image));
+        BufferedImage newBi = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_USHORT_GRAY);
+        newBi.getGraphics().drawImage(bi, 0, 0, null);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ImageIO.write(newBi, "png", os);
+        return ImageDataFactory.create(os.toByteArray());
+    }
 }
