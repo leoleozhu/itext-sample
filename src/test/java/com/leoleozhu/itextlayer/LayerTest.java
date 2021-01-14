345977fc65fa607a9552cdf707607690d883e6e7
Leo Zhu <Leiping.Zhu@gmail.com>
2021-01-14 11:53:12 +0800

    update Layered pdf sample

--- src/test/java/com/leoleozhu/itextlayer/LayerTest.java
+++ src/test/java/com/leoleozhu/itextlayer/LayerTest.java
@@ -1,10 +1,12 @@
 package com.leoleozhu.itextlayer;
 
 import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.colors.Color;
 import com.itextpdf.kernel.colors.DeviceRgb;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfWriter;
@@ -27,12 +29,11 @@ public class LayerTest extends TestCaseBase {
     protected void setUp() throws Exception {
         super.setUp();
 
-        try(InputStream fontInputStream = new FileInputStream(resourceFile("fonts/SourceHanSansCN-Bold.otf"))) {
+        try (InputStream fontInputStream = new FileInputStream(resourceFile("fonts/SourceHanSansCN-Bold.otf"))) {
             ByteArrayOutputStream fontStream = new ByteArrayOutputStream();
             IOUtils.copy(fontInputStream, fontStream);
             font = PdfFontFactory.createFont(fontStream.toByteArray(), PdfEncodings.IDENTITY_H, true);
         }
-
 
         fontsize = 12;
     }
@@ -43,35 +44,56 @@ public class LayerTest extends TestCaseBase {
 
         String destination = targetFile("layer-MultipleLayerPdf.pdf");
         PdfDocument pdfDocument = new PdfDocument(new PdfWriter(destination));
-        PdfPage page = pdfDocument.addNewPage(new PageSize(mm2pt(160f), mm2pt(160f)));
-
-        PdfCanvas pdfCanvas = new PdfCanvas(page);
 
         // layers
         PdfLayer shapeLayer = new PdfLayer("Shape", pdfDocument);
         PdfLayer txtLayer = new PdfLayer("Text", pdfDocument);
 
-        // shapes
-        pdfCanvas.beginLayer(shapeLayer);
+        for (int i = 0; i < 2; i++) {
+            PdfPage page = pdfDocument.addNewPage(new PageSize(mm2pt(160f), mm2pt(160f)));
+            addRectToPageLayer(page, shapeLayer, new DeviceRgb(0, 0, 100));
+            addTextToPageLayer(page, txtLayer, "Page " + (i + 1), new DeviceRgb(150, 150, 0));
+        }
 
-        pdfCanvas.setFillColor( new DeviceRgb(0, 0, 200)).setStrokeColor( new DeviceRgb(0, 0, 200));
-        pdfCanvas.rectangle(mm2pt(40f), mm2pt(40f), mm2pt(80f), mm2pt(80f)).setLineWidth(mm2pt(1)).stroke();
+        // close doc
+        pdfDocument.close();
+    }
+
+    private void addRectToPageLayer(PdfPage page, PdfLayer layer, Color color) {
+        PdfCanvas pdfCanvas = new PdfCanvas(page);
+        pdfCanvas.beginLayer(layer);
+
+        pdfCanvas.setFillColor(color)
+                .setStrokeColor(color);
+
+        Rectangle pageRect = page.getPageSize();
+        float margin = Math.min(pageRect.getWidth(), pageRect.getHeight()) * 0.1f;
+
+        pdfCanvas.rectangle(
+                pageRect.getX() + margin,
+                pageRect.getY() + margin,
+                pageRect.getWidth() - 2 * margin,
+                pageRect.getHeight() - 2 * margin)
+                .setLineWidth(mm2pt(1)).stroke();
 
         pdfCanvas.endLayer();
+    }
 
-        // text
-        pdfCanvas.beginLayer(txtLayer);
+    private void addTextToPageLayer(PdfPage page, PdfLayer layer, String text, Color color) {
+        PdfCanvas pdfCanvas = new PdfCanvas(page);
+        pdfCanvas.beginLayer(layer);
 
-        pdfCanvas.setFillColor(new DeviceRgb(200, 200, 0)).setStrokeColor(new DeviceRgb(200, 200, 0));
-        pdfCanvas.rectangle(mm2pt(10f), mm2pt(10f), mm2pt(140f), mm2pt(20f)).setLineWidth(mm2pt(1)).stroke();
-        pdfCanvas.beginText().moveText(mm2pt(40f), mm2pt(20f))
+        pdfCanvas.setFillColor(color)
+                .setStrokeColor(color);
+
+        Rectangle pageRect = page.getPageSize();
+        float margin = Math.min(pageRect.getWidth(), pageRect.getHeight()) * 0.3f;
+
+        pdfCanvas.beginText().moveText(pageRect.getX() + margin, pageRect.getY() + margin)
                 .setFontAndSize(font, fontsize)
                 .showText(text)
                 .endText();
 
         pdfCanvas.endLayer();
-
-        // close doc
-        pdfDocument.close();
     }
 }
